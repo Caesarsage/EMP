c35f5d19f7631b55e437b37075d550562ef6b85b
Destiny Erhabor <destinterhabor6@gmail.com>
2020-11-28 10:24:48 +0100

    Initial setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const MethodOverride = require('method-override');
+const chalk = require('chalk');
+const ejsMate = require('ejs-mate');
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('views engine', 'ejs');
+app.set('views', path.join(__dirname , '/views'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(MethodOverride('_method'));
+
+
+
+// Listening
+const PORT = process.env.PORT || 3000;
+
+app.listen(()=>{
+  console.log(`connecting at port ${chalk.yellow(PORT)}`);
+})
