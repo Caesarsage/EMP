fea8745d8a252df9f3821411bef5122fa7f1dc9e
Destiny Erhabor <destinterhabor6@gmail.com>
2020-11-28 11:01:06 +0100

    creating views partials and boilerplate

--- index.js
+++ index.js
@@ -4,18 +4,26 @@ const MethodOverride = require('method-override');
 const chalk = require('chalk');
 const ejsMate = require('ejs-mate');
 
+const adminRoute = require('./routes/adminRoute');
+
 const app = express();
 
 app.engine('ejs', ejsMate);
-app.set('views engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname , '/views'));
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/node_modules/font-awesome')))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(MethodOverride('_method'));
 
+app.get('/', (req, res)=>{
+  res.redirect('/admin')
+})
 
+// Routes
+app.use('/admin', adminRoute);
 
 // Listening
 const PORT = process.env.PORT || 3000;
