c0a7e012b0b2dd0c6493c67337d9d11e1e5a3521
Destiny Erhabor <destinterhabor6@gmail.com>
2020-12-04 13:27:50 +0100

    Jobs POST request

--- routes/jobListRoute.js
+++ routes/jobListRoute.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const Job = require('../model/jobListModel');
 
+const catchAsync = require('../utils/asyncCatch');
+
 const router = express.Router();
 
 router.route('/')
-.get( async(req, res)=>{
+.get(catchAsync(async(req, res)=>{
   const jobs = await Job.find({});
   res.render('jobsList/show',{
     jobs
   }
 )
-})
+}))
+.post( catchAsync(async(req, res)=>{
+  const {name, description, validThrough, employmentType, baseSalary} = req.body;
+  const jobs = new Job({
+    name,
+    description,
+    validThrough, 
+    employmentType, 
+    baseSalary
+  });
+  await jobs.save();
+  res.redirect(`/admin/jobs`);
+}))
 
 
 
